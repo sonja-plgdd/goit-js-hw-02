b4966bc3e8b207ee2bc690bdd4263065dddacbfc
sonja <signtieva@gmail.com>
2024-08-29 20:34:23 +0300

    add js

---
+++ js/task-3.js
@@ -0,0 +1,26 @@
+'use strict'
+
+function checkForSpam(message) {            
+    const triggerWord1 = 'spam';
+    const triggerWord2 = 'sale';
+    const lowerCaseMessage = message.toLowerCase();
+    if (lowerCaseMessage.includes(triggerWord1)) {
+        return true;
+    }
+    else if (lowerCaseMessage.includes(triggerWord2)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
